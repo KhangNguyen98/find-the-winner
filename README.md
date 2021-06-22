48face754ea59e8982ff17a3c069257f6b8bca3d
KhangNguyen98 <khanghynguyen48@gmail.com>
2021-06-22 18:43:34 +0700

    1st Commit.Hehe

---
+++ README.md
@@ -0,0 +1,11 @@
+### Hi 👋, This project is just basic and focus on
+
+- 👌 &nbsp; JavaScript -> Learn from it then makes me feel exicted about learning Angular & React 🤩
+
+- 😁 &nbsp; HTML : practice with local storage and some basic tag.
+
+- 🔥 &nbsp; CSS : use CSS invalid sector 😆.
+
+<hr>
+
+<p>This is my first project with Javascript, so it's just tiny project 🥺. But hoping u enjoy it 🥰</p>
---
+++ js/player.js
@@ -0,0 +1,21 @@
+console.log("hello from player.js");
+
+//export
+class Player{
+  constructor(name, score){
+    this.name = name;
+    this.score = score;
+  }
+
+  toString() {
+   return `Player | name : ${this.name} | age : ${this.score}`;
+  }
+}
+
+// function Player(name, score){
+//  this.name = name;
+//  this.score = score;
+// }
+
+//new way to export
+// module.exports =  Player;
